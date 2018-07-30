b9ee77b9653f7642b4a77b6cc29d27fee98adc14
okunishinishi <okunishinishi@gmail.com>
2018-07-30 18:31:50 +0900

    add error handling

--- src/socketioconnection.js
+++ src/socketioconnection.js
@@ -1,23 +1,23 @@
-var io = require('socket.io-client');
+var io = require('socket.io-client')
 
-function SocketIoConnection(config) {
-    this.connection = io.connect(config.url, config.socketio);
+function SocketIoConnection (config) {
+  this.connection = io.connect(config.url, config.socketio)
 }
 
 SocketIoConnection.prototype.on = function (ev, fn) {
-    this.connection.on(ev, fn);
-};
+  this.connection.on(ev, fn)
+}
 
 SocketIoConnection.prototype.emit = function () {
-    this.connection.emit.apply(this.connection, arguments);
-};
+  this.connection.emit.apply(this.connection, arguments)
+}
 
 SocketIoConnection.prototype.getSessionid = function () {
-    return this.connection.id;
-};
+  return this.connection.id
+}
 
 SocketIoConnection.prototype.disconnect = function () {
-    return this.connection.disconnect();
-};
+  return this.connection.disconnect()
+}
 
-module.exports = SocketIoConnection;
+module.exports = SocketIoConnection
